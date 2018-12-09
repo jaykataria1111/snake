ed63eea41445ad702d54b8781232b0c9936867f7
nithillsethupathi <nithillsethupathi@gmail.com>
2018-12-08 23:11:17 -0800

    Added MessageTest and LeftButtonMessageTest JUnit

---
+++ Snake/src/message/LeftButtonMessageTest.java
@@ -0,0 +1,14 @@
+package message;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LeftButtonMessageTest {
+
+	@Test
+	void testLeftButtonMessage() {
+		
+	}
+
+}
---
+++ Snake/src/message/MessageTest.java
@@ -0,0 +1,14 @@
+package message;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MessageTest {
+
+	@Test
+	void testMessage() {
+		
+	}
+
+}
